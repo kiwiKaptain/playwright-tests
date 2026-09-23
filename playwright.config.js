67873076318451da37f5c7b74ac7b4f42be834f2
Sharad Maurya <55784109+kiwiKaptain@users.noreply.github.com>
2026-09-23 19:32:41 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,24 @@
-module.exports = {
+const { defineConfig } = require("@playwright/test");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const envPath = path.join(__dirname, ".env");
+
+dotenv.config({
+    path: envPath, quiet: true
+});
+
+module.exports = defineConfig({
     testDir: "./tests",
 
     use: {
         browserName: "chromium",
         headless: false,
+        baseURL: process.env.BASE_URL,
+        screenshot: "on",
+        video: "on",
+        trace: "on-first-retry",
     },
 
     reporter: [["list"], ["html", { open: "never" }]],
-};
+});
